86efd7d5a3672625f1734089c22287857c12056f
Nils Nahooy <nils.nahooy@live.com>
2024-01-31 22:25:09 +0100

    put sprite on screen

--- Game.cpp
+++ Game.cpp
@@ -1,4 +1,7 @@
-#include "Game.hpp"
+#include "Game.h"
+
+SDL_Texture *playerTexture;
+SDL_Rect srcR, destR;
 
 Game::Game()
 {
@@ -36,6 +39,10 @@ void Game::init(const char *title, int xpos, int ypos, int width, int height, bo
     {
         isRunning = false;
     }
+
+    SDL_Surface *tmpSurface = IMG_Load("assets/player_sprite.png");
+    playerTexture = SDL_CreateTextureFromSurface(renderer, tmpSurface);
+    SDL_FreeSurface(tmpSurface);
 }
 
 void Game::handleEvents()
@@ -56,13 +63,17 @@ void Game::handleEvents()
 void Game::update()
 {
     cnt++;
+    destR.h = 64;
+    destR.w = 64;
+    destR.x = cnt;
     std::cout << cnt << std::endl;
 }
 
 void Game::render()
 {
     SDL_RenderClear(renderer);
-    // TODO: Add content to render
+    // Add content to render, content is rendered from back to front
+    SDL_RenderCopy(renderer, playerTexture, NULL, &destR);
     SDL_RenderPresent(renderer);
 }
 
--- Makefile
+++ Makefile
@@ -16,7 +16,7 @@ LIBRARY_PATHS = -L src/lib
 COMPILER_FLAGS = -w
 
 #LINKER_FLAGS specifies the libraries we're linking against
-LINKER_FLAGS = -lmingw32 -lSDL2main -lSDL2
+LINKER_FLAGS = -lmingw32 -lSDL2main -lSDL2 -lSDL2_image
 
 #OBJ_NAME specifies the name of our exectuable
 OBJ_NAME = main
