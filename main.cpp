0b093e5eb63573d8114c985126d260cad704302c
Nils Nahooy <nils.nahooy@live.com>
2024-02-01 12:29:13 +0100

    set FPS to 60

--- main.cpp
+++ main.cpp
@@ -4,13 +4,28 @@ Game *game = nullptr;
 
 int main(int argc, char *argv[])
 {
+    const int TARGETFPS = 60;
+    const int FRAMEDELAY = 1000 / TARGETFPS;
+
+    Uint64 frameStart;
+    int frameTime;
+
     game = new Game();
     game->init("Ursus Engine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false);
     while (game->running())
     {
+        frameStart = SDL_GetTicks64();
+
         game->handleEvents();
         game->update();
         game->render();
+
+        frameTime = SDL_GetTicks64() - frameStart;
+
+        if (FRAMEDELAY > frameTime)
+        {
+            SDL_Delay(FRAMEDELAY - frameTime);
+        }
     }
     game->clean();
     return 0;
