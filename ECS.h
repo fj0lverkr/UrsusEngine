991e12be4939ea0aa7b056ac688481b0a235009d
Nils Nahooy <nils.nahooy@live.com>
2024-02-02 17:45:23 +0100

    attempt at getting it to work on linux...

--- ECS.h
+++ ECS.h
@@ -1,28 +1,25 @@
 #pragma once
 
-#include <iostream>
-#include <vector>
-#include <memory>
 #include <algorithm>
-#include <bitset>
 #include <array>
+#include <bitset>
+#include <iostream>
+#include <memory>
+#include <vector>
 
 class Component;
 class Entity;
 
 using ComponentId = std::size_t;
 
-inline ComponentId getComponentTypeId()
-{
-    static ComponentId lastId = 0;
-    return lastId++;
+inline ComponentId getComponentTypeId() {
+  static ComponentId lastId = 0;
+  return lastId++;
 };
 
-template <typename T>
-inline ComponentId getComponentTypeId() noexcept
-{
-    static ComponentId typeId = getComponentTypeId();
-    return typeId;
+template <typename T> inline ComponentId getComponentTypeId() noexcept {
+  static ComponentId typeId = getComponentTypeId();
+  return typeId;
 }
 
 constexpr std::size_t maxComponents = 32;
@@ -30,72 +27,59 @@ constexpr std::size_t maxComponents = 32;
 using ComponentBitset = std::bitset<maxComponents>;
 using ComponentArray = std::array<Component *, maxComponents>;
 
-class Component
-{
+class Component {
 public:
-    Entity *entity;
+  Entity *entity;
 
-    virtual void init(){};
-    virtual void update(){};
-    virtual void draw(){};
+  virtual void init(){};
+  virtual void update(){};
+  virtual void draw(){};
 
-    virtual ~Component(){};
+  virtual ~Component(){};
 };
 
-class Entity
-{
+class Entity {
 private:
-    bool active = true;
-    std::vector<std::unique_ptr<Component>> components;
-    ComponentArray componentArray;
-    ComponentBitset componentBitset;
+  bool active = true;
+  std::vector<std::unique_ptr<Component>> components;
+  ComponentArray componentArray;
+  ComponentBitset componentBitset;
 
 public:
-    void update()
-    {
-        for (auto &c : components)
-            c->update();
-        for (auto &c : components)
-            c->draw();
-    }
+  void update() {
+    for (auto &c : components)
+      c->update();
+    for (auto &c : components)
+      c->draw();
+  }
 
-    void draw()
-    {
-    }
+  void draw() {}
 
-    bool isActive() const { return active; }
+  bool isActive() const { return active; }
 
-    void destroy() { active = false; }
+  void destroy() { active = false; }
 
-    template <typename T>
-    bool hasComponent() const
-    {
-        return ComponentBitset[getComponentId<T>];
-    }
+  template <typename T> bool hasComponent() const {
+    return componentBitset[getComponentTypeId<T>];
+  }
 
-    template <typename T, typename... TArgs>
-    T &addComponent(TArgs &&...mArgs)
-    {
-        T *c(new T(std::forward<TArgs>(mArgs)...));
-        c->entity = this;
-        std::unique_ptr<Component> uPtr{c};
-        components.emplace_back(std::move(uPtr));
+  template <typename T, typename... TArgs> T &addComponent(TArgs &&...mArgs) {
+    T *c(new T(std::forward<TArgs>(mArgs)...));
+    c->entity = this;
+    std::unique_ptr<Component> uPtr{c};
+    components.emplace_back(std::move(uPtr));
 
-        ComponentArray[getComponentTypeId<T>()] = c;
-        ComponentBitset[getComponentTypeId<T>()] = true;
+    componentArray[getComponentTypeId<T>()] = c;
+    componentBitset[getComponentTypeId<T>()] = true;
 
-        c->init();
-        return *c;
-    }
+    c->init();
+    return *c;
+  }
 
-    template <typename T>
-    T &GetComponent() const
-    {
-        auto ptr(componentArray[getComponentTypeId<T>()]);
-        return *static_cast<T *>(ptr);
-    }
+  template <typename T> T &GetComponent() const {
+    auto ptr(componentArray[getComponentTypeId<T>()]);
+    return *static_cast<T *>(ptr);
+  }
 };
 
-class Manager
-{
-};
+class Manager {};
