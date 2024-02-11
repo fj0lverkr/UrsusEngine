c40706612211d73dca84053496afb49f6468f5a0
Nils Nahooy <nils.nahooy@live.com>
2024-02-11 17:24:19 +0100

    re-add tiled project

---
+++ assets/tiled/map_tiles_sheet.tsx
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="map_tiles_sheet" tilewidth="32" tileheight="32" tilecount="100" columns="10">
+ <image source="map_tiles_sheet.png" width="320" height="320"/>
+ <tile id="30">
+  <objectgroup draworder="index" id="3">
+   <object id="15" x="2.4375" y="0">
+    <polygon points="0,0 0,2.875 1.75,4.625 0.9375,6.1875 0.0625,7.3125 -0.0625,14.75 1.625,16.25 0.3125,17.4375 -0.0625,19.125 0.125,23.75 2.125,26.3125 2.875,27.875 6.125,28 8.25,29.1875 11.6875,29.5 13.625,28.5 14.1875,27.8125 15.4375,29.125 16.4375,29.8125 20,29.5 23.0625,26.875 24.875,28.5625 25.8125,29.625 29.5,29.5625 29.4375,31.0625 -1.9375,30.3125 -1.75,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
