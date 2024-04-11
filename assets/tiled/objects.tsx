054eb978d1c0a6fafa5ac1c5fb2b414b4d812812
Nils Nahooy <nils.nahooy@live.com>
2024-04-11 22:21:40 +0200

    test Ysorting option on objects

--- assets/tiled/objects.tsx
+++ assets/tiled/objects.tsx
@@ -1,10 +1,10 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.2" name="objects" tilewidth="128" tileheight="128" tilecount="1" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
-        <tile id="4">
-            <properties>
-                <property name="Ysorting" type="bool" value="true" />
-            </properties>
+ <tile id="4">
+  <properties>
+   <property name="Ysorting" type="bool" value="true"/>
+  </properties>
   <image width="128" height="128" source="objects/Ash-10.png"/>
   <objectgroup draworder="index" id="3">
    <object id="2" type="ObjectCollision" x="57.6364" y="128">
