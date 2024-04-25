7f51b398ea0e37eba8c6da5f05cc4d35ffdbe9b2
Nils Nahooy <nils.nahooy@live.com>
2024-04-25 08:37:13 +0200

    Create stub for AnimatedTileComponent class.

---
+++ src/ECS/AnimatedTileComponent.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "ECS.hpp"
+#include "TileComponent.hpp"
+#include "Game.hpp"
+#include <SDL2/SDL.h>
+
+class AnimatedTileComponent : public TileComponent
+{
+public:
+    ~AnimatedTileComponent() {}
+
+    AnimatedTileComponent(int srcX, int srcY, float posX, float posY, std::string assetId, int tileSize, int scaleFactor)
+    {
+        TileComponent::TileComponent(srcX, srcY, posX, posY, assetId, tileSize, scaleFactor);
+    }
+
+    void update()
+    {
+        TileComponent::update();
+    }
+
+    void draw()
+    {
+        TileComponent::draw();
+    }
+};
--- src/ECS/Components.hpp
+++ src/ECS/Components.hpp
@@ -7,6 +7,7 @@
 #include "PolygonLinesComponent.hpp"
 #include "KeyboardController.hpp"
 #include "ColliderComponent.hpp"
+#include "AnimatedTileComponent.hpp"
 #include "TileComponent.hpp"
 #include "AnchorComponent.hpp"
 #include "YSortingComponent.hpp"
