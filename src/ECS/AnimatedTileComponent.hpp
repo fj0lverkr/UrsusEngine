27edf100dbc2e031b51ad2b7053d06217a918c73
Nils Nahooy <nils.nahooy@live.com>
2024-04-27 19:16:59 +0200

    set up frames for animation

--- src/ECS/AnimatedTileComponent.hpp
+++ src/ECS/AnimatedTileComponent.hpp
@@ -5,19 +5,44 @@
 #include "Game.hpp"
 #include <SDL2/SDL.h>
 
+using namespace std;
+
+struct AnimatedTileFrame
+{
+    string assetId;
+    int duration;
+
+    ~AnimatedTileFrame() {};
+
+    AnimatedTileFrame(string assetId, int duration)
+    {
+        this->assetId = assetId;
+        this->duration = duration;
+    }
+};
+
 class AnimatedTileComponent : public TileComponent
 {
+private:
+    int currentFrameId = 0;
+    vector<AnimatedTileFrame> tileFrames;
 public:
     ~AnimatedTileComponent() {}
 
-    AnimatedTileComponent(int srcX, int srcY, float posX, float posY, std::string assetId, int tileSize, int scaleFactor)
+    AnimatedTileComponent(int srcX, int srcY, float posX, float posY, int tileSize, int scaleFactor, vector<AnimatedTileFrame> frames)
     {
-        TileComponent(srcX, srcY, posX, posY, assetId, tileSize, scaleFactor);
+        tileFrames = frames;
+        TileComponent(srcX, srcY, posX, posY, tileFrames[currentFrameId].assetId, tileSize, scaleFactor);
     }
 
     void update()
     {
         TileComponent::update();
+    }
+
+    void swapFrame(int frameId)
+    {
+        texture = Game::assets->GetTexture(tileFrames[frameId].assetId);
     }
 
     void draw()
